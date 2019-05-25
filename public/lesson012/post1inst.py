5eb5774ae255ffa1a0389900e788b088f0fb23a6
Dan Bikle <danbikle@outlook.com>
2019-05-25 14:50:09 -0700

    more

--- public/lesson012/post1inst.py
+++ public/lesson012/post1inst.py
@@ -3,6 +3,18 @@ post1inst.py
 
 This script should use the REST API to create a GCE instance.
 
+With my mouse, I should get a JSON string from this page:
+https://console.cloud.google.com/compute/instancesAdd
+Then, I should paste the string into a file:
+cr1inst.json
+
+With my mouse, I should get an API_KEY from this page:
+https://console.cloud.google.com/apis/credentials
+Then, I should paste the string into a file:
+api_key.txt
+
+Then, this Python script should have a hope.
+
 Demo:
 python3 post1inst.py
 """
@@ -10,26 +22,15 @@ python3 post1inst.py
 import re
 import requests
 
-# With my mouse, I should get a JSON string from this page:
-# https://console.cloud.google.com/compute/instancesAdd
-# Then, I should paste the string into a file:
-# cr1inst.json
-
-# With my mouse, I should get an API_KEY from this page:
-# https://console.cloud.google.com/apis/credentials
-# Then, I should paste the string into a file:
-# api_key.txt
-
-# Then, this Python script should have a hope.
-
 # I should load the JSON file into a string:
 with open('cr1inst.json') as fh:
   jsonf_s = fh.read()
 
 # I should use a regexp to get the POST-URL:
 matches    = re.search('(POST )(https.*instances)', jsonf_s)
-line1_s    = matches.group(0)
+line1_s    = matches.group(0) # s.b. Useful later
 post_url_s = matches.group(2)
+print('I will POST a request to this URL:')
 print(post_url_s)
 
 # I should get the API_KEY string from api_key.txt
@@ -39,9 +40,10 @@ print(post_url_s)
 with open('api_key.txt') as fh:
   api_key_s = fh.read()
 
-params_d   = {'key':api_key_s}
-post_url_s = 'http://localhost:3101/home/takepost'
+params_d   = {'key': api_key_s}
+# post_url_s = 'http://localhost:3101/home/takepost'
 json_s     = jsonf_s.replace(line1_s,'')
-json_s     = '{"hello": "world"}' # for now
+# json_s     = '{"hello": "world"}' # for now
 # I should POST my JSON to the URL listed in line 1 of the JSON file:
-req    = requests.post(post_url_s, params=params_d, json=json_s)
+req = requests.post(post_url_s, params=params_d, json=json_s)
+print('Done')
