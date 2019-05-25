817faab8bb331382c8f1c7a8bbc4b4a8052ea0f3
Dan Bikle <danbikle@outlook.com>
2019-05-24 22:29:42 -0700

    more

--- public/lesson012/cr5inst.py
+++ public/lesson012/cr5inst.py
@@ -7,4 +7,17 @@ Demo:
 python3 cr5inst.py
 """
 
+import subprocess
 
+# I should read gcloud10.bash into a string:
+with open('gcloud10.bash') as fh:
+    my_s = fh.read()
+
+# I should create 5 scripts from names of states:
+for instance_name_s in ['calif', 'tx', 'newyork', 'mass', 'colo']:
+    ur_s     = my_s.replace('instance-2',instance_name_s).replace('gcloud10',instance_name_s)
+    script_s = '/tmp/' + instance_name_s + '.bash' 
+    with open(script_s, 'w') as fh:
+        fh.write(ur_s)
+    # I should run each script:
+    subprocess.run(["/bin/bash", script_s])    
