a48b6dce97e7ab8affc792783dd1a6a0dfd18f90
Dan Bikle <danbikle@outlook.com>
2019-05-27 14:32:01 -0700

    more

--- public/lesson012/gcp2.py
+++ public/lesson012/gcp2.py
@@ -11,21 +11,18 @@ python3 gcp2.py
 import googleapiclient.discovery
 compute   = googleapiclient.discovery.build('compute', 'v1')
 project_s = 'shining-sol-241621'
+name_s    = 'linuxbox5'
 zone_s    = 'us-central1-a'
-name_s    = 'linuxbox2'
+machine_type_s = "zones/"+zone_s+"/machineTypes/n1-standard-1"
 
 # Get the latest Debian Jessie image.
 image_response = compute.images().getFromFamily(
     project='debian-cloud', family='debian-9').execute()
 source_disk_image = image_response['selfLink']
 
-# Configure the machine
-machine_type_s = "zones/%s/machineTypes/n1-standard-1" % zone_s
-
 config = {
-    'name': name_s,
+    'name':        name_s,
     'machineType': machine_type_s,
-
     # Specify the boot disk and the image to use as a source.
     'disks': [
         {
@@ -36,22 +33,12 @@ config = {
             }
         }
     ],
-
     # Specify a network interface with NAT to access the public
     # internet.
     'networkInterfaces': [{
         'network': 'global/networks/default',
         'accessConfigs': [
             {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-        ]
-    }],
-
-    # Allow the instance to access cloud storage and logging.
-    'serviceAccounts': [{
-        'email': 'default',
-        'scopes': [
-            'https://www.googleapis.com/auth/devstorage.read_write',
-            'https://www.googleapis.com/auth/logging.write'
         ]
     }]
 }
