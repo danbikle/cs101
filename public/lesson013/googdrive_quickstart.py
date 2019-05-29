dfe3d59901a387f20b285e6c9b0296ed06e566f1
Dan Bikle <danbikle@outlook.com>
2019-05-28 19:41:19 -0700

    more

---
+++ public/lesson013/googdrive_quickstart.py
@@ -0,0 +1,18 @@
+"""
+googdrive_quickstart.py
+
+This script should use python3 to talk with Google Drive.
+
+Ref:
+https://developers.google.com/drive/api/v3/quickstart/python
+
+The above URL offers a blue button [ENABLE THE DRIVE API] to get credentials.json
+
+I placed my copy of credentials.json here: $HOME/credentials.json
+
+Demo:
+python3 google_drive_quickstart.py
+"""
+
+
+print('Done')
