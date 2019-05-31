bf59a3f6b67debb8b5489cacc2759fcca8436d11
Dan Bikle <danbikle@outlook.com>
2019-05-30 18:24:58 -0700

    more

--- public/lesson013/googdrive17.py
+++ public/lesson013/googdrive17.py
@@ -3,27 +3,31 @@ googdrive17.py
 
 This script should delete files named 'hello.txt'
 
-Demo:
+Ref:
+https://developers.google.com/drive/api/v3/quickstart/python
+
+Demo (Ubuntu):
+sudo apt install python3-pip
+sudo pip3 install --upgrade google-api-python-client
+sudo pip3 install --upgrade google-auth-httplib2
+sudo pip3 install --upgrade google-auth-oauthlib
+
 python3 googdrive17.py
 """
 
-from googleapiclient.discovery    import build
-#from googleapiclient.http         import MediaFileUpload
-import os
-
 import pickle
 import os.path
+from googleapiclient.discovery      import build
+from googleapiclient.http           import MediaFileUpload
 from google_auth_oauthlib.flow      import InstalledAppFlow
 from google.auth.transport.requests import Request
-
 
 # I s.declare a very permissive scope (for training only):
 SCOPES      = ['https://www.googleapis.com/auth/drive']
 
 creds = None
 # The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
+# created automatically when the authorization flow completes for the first time.
 if os.path.exists('token.pickle'):
     with open('token.pickle', 'rb') as fh:
         creds = pickle.load(fh)
@@ -38,34 +42,45 @@ if not creds or not creds.valid:
     # Save the credentials for the next run
     with open('token.pickle', 'wb') as token:
         pickle.dump(creds, token)
-'''
 
+# I s.create a file so I can upload it:
+with open('/tmp/hello.txt','w') as fh:
+    fh.write("hello world\n")
+# From my laptop, I s.upload a file named hello.txt:
 drive_service = build('drive', 'v3', credentials=creds)
+file_metadata = {'name': 'hello.txt'}
+media = MediaFileUpload('/tmp/hello.txt', mimetype='text/plain')
+create_response = drive_service.files().create(body=file_metadata,
+                                     media_body=media,
+                                     fields='id').execute()
+file_id = create_response.get('id')
+print('new /tmp/hello.txt file_id:')
+print(file_id)
 
-# Q: With googleapiclient, how to filter files list results?
+# Q: With googleapiclient, how to filter files list()-response?
 # A1: https://developers.google.com/drive/api/v3/reference/files/list
 # A2: https://developers.google.com/drive/api/v3/search-files
 
-list_results = drive_service.files().list(
-    orderBy  = "createdTime desc",
-    q        = "name='hello.txt'",
-    pageSize = 22,
-    fields   = "files(id, name)"
+list_response = drive_service.files().list(
+    orderBy   = "createdTime desc",
+    q         = "name='hello.txt'",
+    pageSize  = 22,
+    fields    = "files(id, name)"
 ).execute()
 
-items = list_results.get('files', [])
+items = list_response.get('files', [])
 
 if items:
-    print('Files I want to delete:')
     for item in items:
-        print('I will attempt to delete this file:')
+        print('I will try to delete this file:')
         print(u'{0} ({1})'.format(item['name'], item['id']))
-        del_results = drive_service.files().delete(fileId=item['id'])
-        print(del_results.body)
-    print('I will attempt to emptyTrash:')
-    trash_results = drive_service.files().emptyTrash()
-    print('trash_results.body:')
-    print( trash_results.body)
+        del_response = drive_service.files().delete(fileId=item['id'])
+        print('del_response.body:')
+        print( del_response.body)
+    print('I will try to emptyTrash:')
+    trash_response = drive_service.files().emptyTrash()
+    print('trash_response.body:')
+    print( trash_response.body)
 else:
-    print('No files found.')
-'''
+    print('hello.txt not found in your google-drive account.')
+
