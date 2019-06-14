1a1e60cc654abc933e2f5a318a66d0192249aca6
Dan Bikle <danbikle@outlook.com>
2019-06-13 19:11:54 -0700

    more

--- public/lesson016/sheet13.py
+++ public/lesson016/sheet13.py
@@ -27,33 +27,32 @@ service           = build('sheets', 'v4', credentials=creds)
 field_s           = 'spreadsheetUrl,spreadsheetId'
 body_d            = {'properties':{'title':'lesson016sheetB'}}
 response_ofcreate = service.spreadsheets().create(fields=field_s, body=body_d).execute()
-response_ofget    = response_ofcreate.get('spreadsheetId')
+spreadsheet_id    = response_ofcreate.get('spreadsheetId')
 print('I just created spreadsheet; it has an ID:')
-print(response_ofget)
-spreadsheet_id = response_ofget
+print(spreadsheet_id)
+spreadsheet_url = response_ofcreate.get('spreadsheetUrl')
 print('spreadsheetUrl:')
-print( spreadsheetUrl  )
+print( spreadsheet_url)
 
 # To write data to a spreadsheet, I need this:
 # spreadsheetId
 # The range in A1 notation
 # A nested list full of values
 
-range_s        = 'Sheet1!A1:C'
-range_s        = 'Sheet1!A1'
+range_s        = 'Sheet1!A1' # The range in A1 notation
 
 # I shd use a spreadsheets.values.update request to write data to a single range
 
 row1_l   = [1.1, 2.1, 3.3]
 row2_l   = [1.2, 2.3, 3.1]
-values_l = [row1_l, row2_l]
+values_l = [row1_l, row2_l] # A nested list full of values
 body     = {
     'values': values_l
 }
 
 response_ofupdate = service.spreadsheets().values().update(
     spreadsheetId=spreadsheet_id, range=range_s,
-    valueInputOption='RAW', body=body).execute()
+    valueInputOption='USER_ENTERED', body=body).execute()
 
 print("response_ofupdate.get('updatedCells'):")
 print( response_ofupdate.get('updatedCells')  )
