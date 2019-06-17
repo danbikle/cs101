dec9927f521d532975e321070528679b03f59a93
Dan Bikle <danbikle@outlook.com>
2019-06-16 23:49:09 -0700

    more

--- public/lesson017/demo10.py
+++ public/lesson017/demo10.py
@@ -11,3 +11,28 @@ This script should:
 Demo:
 python3 demo10.py
 """
+
+import datetime
+import os
+import pickle
+from googleapiclient.discovery    import build
+from oauth2client.service_account import ServiceAccountCredentials
+from httplib2                     import Http
+
+secretf_s = os.environ['HOME']+'/secret0616.json'
+
+with open(secretf_s) as fh:
+  json_s = fh.read()
+
+print('json_s[:66]:')
+print( json_s[:66])
+
+# I s.declare a very permissive scope (for training only):
+SCOPES      = ['https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(secretf_s,
+                                                               SCOPES)
+http_auth = credentials.authorize(Http())
+service   = build('drive', 'v3', http=http_auth)
+
+print('I should now be authenticated and authorized to use service:')
+print(service)
