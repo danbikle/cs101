a3dfabde459eba602cd5dba185d369619e487dc3
Dan Bikle <danbikle@outlook.com>
2019-06-16 17:48:24 -0700

    more

--- public/lesson016/sheet24.py
+++ public/lesson016/sheet24.py
@@ -13,6 +13,7 @@ import datetime
 import os
 import pickle
 from googleapiclient.discovery    import build
+from googleapiclient.http         import MediaFileUpload
 from oauth2client.service_account import ServiceAccountCredentials
 from httplib2                     import Http
 
@@ -50,3 +51,31 @@ drive_service = build('drive', 'v3', http=http_auth)
 
 print('drive_service:')
 print( drive_service)
+
+# I shd create /tmp/hello.txt on Linux:
+with open('/tmp/hello.txt','w') as fh:
+    fh.write("hello world\n")
+
+# I shd copy hello.txt to Google Drive.
+file_metadata = {'name': 'hello.txt'}
+media = MediaFileUpload('/tmp/hello.txt', mimetype='text/plain')
+print('I will try to upload /tmp/hello.txt to google drive:')
+create_response = drive_service.files().create(body=file_metadata,
+                                     media_body=media,
+                                     fields='id').execute()
+file_id = create_response.get('id')
+print('new /tmp/hello.txt file_id:')
+print(file_id)
+
+perm_response = drive_service.permissions().list(fileId=file_id).execute()
+print('perm_response:')
+print( perm_response)
+
+# I shd grant role of reader to anyone (who wants it):
+newperm_d = {'role': 'reader', 'type': 'anyone'}
+pc_response = drive_service.permissions().create(fileId=file_id,
+                                                   body = newperm_d
+).execute()
+
+print('pc_response:')
+print( pc_response)
