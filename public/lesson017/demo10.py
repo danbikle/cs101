b7b2b52b8c3f26873177903babf57c34405f4d51
Dan Bikle <danbikle@outlook.com>
2019-06-16 23:42:01 -0700

    more

---
+++ public/lesson017/demo10.py
@@ -0,0 +1,13 @@
+"""
+demo10.py
+
+This script should:
+
+- Create a file named hello.txt
+- Copy hello.txt to Google Drive
+- Grant reader-role to anyone (who wants it)
+- Grant writer-role to bikle101@gmail.com
+
+Demo:
+python3 demo10.py
+"""
